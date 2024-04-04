01de72cf8b8c8c58dae582392be3647a24a6c0a8
Martin George <marty@dingo.foo>
2024-04-04 16:33:00 +0000

    commits of mermaid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,24 +4,12 @@ import MermaidModal from "@/components/ui/blog/MermaidModal";
 
 export default function Home() {
 
-  console.log(process.cwd())
-
-
-
-  const mermaidCode = `%%{init:{'flowchart':{'defaultRenderer':'elk'}}}%%
-  flowchart
-  direction RL
-  
-      Start --> Stop
-  `
-  console.log(JSON.stringify(mermaidCode, null, 2))
   return (
     <div>
       <NavigationBar />
       <div className="prose dark:prose-invert mx-auto container">
-        <pre id="mermaid-123" className="mermaid">
-            {mermaidCode}
-        </pre>
+
+
         <MermaidModal mermaidId="mermaid-123" />
       </div>
     </div>
