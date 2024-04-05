9396a20efa2196092f477d1711c8d7e40bb3a87f
Martin George <marty@dingo.foo>
2024-04-05 11:57:52 +0000

    commits of breadcrumb

--- src/components/ui/navigation/BreadCrumb/BreadCrumb.tsx
+++ src/components/ui/navigation/BreadCrumb/BreadCrumb.tsx
@@ -20,15 +20,32 @@ import Link from "next/link"
 
 import React from "react"
 
+function slugify(text: string) {
+    return text
+        .toString()
+        .toLowerCase()
+        .replace(/\s+/g, "-") // Replace spaces with -
+        .replace(/[^\w-]+/g, "") // Remove all non-word chars
+        .replace(/--+/g, "-") // Replace multiple - with single -
+        .replace(/^-+/, "") // Trim - from start of text
+        .replace(/-+$/, "") // Trim - from end of text
+}
+
 export default function BreadCrumb({ rootPage, curPage, allPosts, curPageData }: { rootPage: { title: string, href: string }, curPage: { title: string, href: string } | undefined, allPosts?: any, curPageData?: any }) {
+    let headings: {"index": number, "title": string, "href": string }[]
     let renderHeadings = false
-    if(curPageData !== undefined ){
+
+    if (curPageData !== undefined) {
         renderHeadings = true
-        const headings = curPageData.content.root.children
-            .filter((childNode) => {childNode.type === "heading" && childNode.tag !== "h1"})
-            .map((heading) => heading.children[0].text)
-        console.log(headings)
+        headings = curPageData.content.root.children
+            .filter((childNode) => childNode.type === "heading" && childNode.tag !== "h1")
+            .map((heading, index) => { return { "index": index, "title": heading.children[0].text, "href": slugify(heading.children[0].text) } })
+            .sort((headingA, headingB) => headingA.index - headingB.index)
     }
+    console.log(headings)
+
+    // React.useState()
+
     return (
         <div className="not-prose">
             <Breadcrumb>
@@ -50,8 +67,30 @@ export default function BreadCrumb({ rootPage, curPage, allPosts, curPageData }:
                                         allPosts
                                             // .filter((post) => post.title !== curPage.title)
                                             .map((post, index) => (
-                                                <DropdownMenuItem  key={`breadcrumb-posts-${index}`}>
+                                                <DropdownMenuItem key={`breadcrumb-posts-${index}`}>
                                                     <Link href={`/blog/${post.slug}`}>{post.title}</Link>
+                                                </DropdownMenuItem>
+                                            ))
+                                    }
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </BreadcrumbItem>
+                    }
+                    {renderHeadings && <BreadcrumbSeparator />}
+                    {renderHeadings &&
+                        <BreadcrumbItem>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger className="flex items-center gap-1">
+                                    {headings.shift().title}
+                                    <ChevronDownIcon />
+                                </DropdownMenuTrigger>
+                                <DropdownMenuContent align="end">
+                                    {
+                                        headings
+                                            // .filter((post) => post.title !== curPage.title)
+                                            .map((heading, index) => (
+                                                <DropdownMenuItem key={`breadcrumb-posts-${index}`}>
+                                                    <Link href={`#${heading.href}`}>{heading.title}</Link>
                                                 </DropdownMenuItem>
                                             ))
                                     }
